08bc87dd633a9e1bea989614618dab36ba1c69e2
Aditya Rawat <adityarawat@mac-tc6ln46q32.ias.catonetworks.com>
2024-06-17 13:50:33 +0200

    [WIP]-Building_application

--- src/app.py
+++ src/app.py
@@ -1,22 +1,31 @@
-import uvicorn
-from fastapi import FastAPI
+from langchain_openai import ChatOpenAI, AzureChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
 
-app = FastAPI()
+import streamlit as st
+import os
+from dotenv import load_dotenv
 
+# from logging import getLogger
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Prompt Template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please response to the user queries"),
+        ("user", "Question:{question}")
+    ]
+)
+# Streamlit Framework
 
+st.title("Langchain Demo with openAI")
+input_text = st.text_input("Search the topic you want")
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-
-def main():
-    uvicorn.run(app, host="0.0.0.0", port=8080)
-
-
-if __name__ == "__main__":
-    main()
+# OpenAI LLM
+llm = AzureChatOpenAI(model_name='azure-gpt-35-turbo',
+                      openai_api_key=os.environ["OPENAI_API_KEY"],
+                      azure_endpoint="https://openai-ds-platform.openai.azure.com/",
+                      openai_api_version="2023-05-15")
